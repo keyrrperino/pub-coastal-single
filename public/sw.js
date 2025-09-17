3364ee1d325443430adaa725d4e1f410cd63c5c3
Keyrr Perino <keyrr@symph.co>
2025-09-17 11:10:41 +0800

    cleaning and optimizing

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,34 @@
-const CACHE_NAME = 'coastal-game-v1';
+const CACHE_NAME = 'coastal-game-v3';
 const urlsToCache = [
   '/',
   '/assets/favicon.png',
   '/assets/logo.png',
+  '/assets/apple-touch-icon.png',
+  '/assets/icon-192x192.png',
+  '/assets/icon-512x512.png',
   '/assets/background.svg',
   '/assets/controller-background.png',
+  '/assets/bg-loading.png',
+  '/assets/Loading Map BG.png',
+  '/assets/Loading Map Overlay.png',
+  '/assets/coin-icon.png',
+  '/assets/tutorial-bg.png',
+  '/assets/arrows-down.svg',
+  '/assets/PUB_RidingTheTides_White.png',
+  '/assets/PUB logo_white_transparent 2.svg',
+  '/assets/map-v2.svg?v=1',
+  '/assets/sector1-highlight-v2.svg',
+  '/assets/sector2-highlight-v2.svg',
+  '/assets/sector3-highlight-v2.svg',
+  '/assets/start-screen-bg-updated.webp',
+  '/assets/land-reclamation-icon-6b707d.png',
+  '/assets/seawall-icon-41fadd.png',
+  '/assets/mangroves-icon-3a15a8.png',
+  '/assets/storm-surge-barrier-icon.png',
+  '/assets/artificial-reef-icon.png',
+  '/assets/hybrid-measure-icon.png',
+  '/assets/modal-arrow.svg',
+  '/assets/input-team-name-bg.png',
   // Add other critical assets that should be cached
 ];
 
--- src/components/tutorial-screens/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TutorialScreen1 } from '../TutorialScreen1';
-export { default as TutorialScreen2 } from '../TutorialScreen2';
-export { default as TutorialScreen3 } from '../TutorialScreen3'; 
